8b9638d9727f1ea090e9938508032fe3088d78ef
Bailon18 <paucarmontesbailon@gmail.com>
2022-10-11 21:34:21 -0500

    Primer repositorio

---
+++ SpringBootIdat/src/main/java/idat/edu/pe/demo/SpringBootIdatApplication.java
@@ -0,0 +1,14 @@
+package idat.edu.pe.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIdatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIdatApplication.class, args);
+		System.out.println("Holitas");
+	}
+
+}
